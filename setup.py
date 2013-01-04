604dd542ba5c8e313fd62cbfa75bd1278216a675
Mercion Wilathgamuwage Don (mwilathg) <mwilathg@cisco.com>
2013-01-04 18:52:40 +0000

    setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+#rom distutils.core import setup
+from setuptools import setup
+
+setup(  name="PyCrash",
+      	version="0.4-pre3",
+		description="PyCrash: a Run-Time Exception Dumper for Python programs",
+		author="Carmine I.D. Noviello, Mercion Wilathgamuwage",
+		author_email="mwilathg@cisco.com",
+		url="http://www.pycrash.org",
+		packages=['pycrash', 'pycrash.utils'])
