0727d0114686f2b7c3d731924f7c86119a33c9eb
Mercion Wilathgamuwage Don (mwilathg) <mwilathg@cisco.com>
2013-01-11 14:26:22 +0000

    Added lxml to setup.py requirements

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(  name="PyCrash",
 		description="PyCrash: a Run-Time Exception Dumper for Python programs",
 		author="Carmine I.D. Noviello, Mercion Wilathgamuwage",
 		author_email="s.mercion@gmail.com",
+        install_requires = ["lxml"],
 		url="http://mercion.github.com/pycrash/docs/",
 		packages=['pycrash', 'pycrash.utils'])
